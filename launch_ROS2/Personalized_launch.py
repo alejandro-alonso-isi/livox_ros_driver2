0542f18db9597c16d3ef4cb78ede67669853b6b4
Alejandro Rolon <alejandro.alonso@senairs.org.br>
2024-08-09 14:17:30 -0300

    style

--- launch_ROS2/Personalized_launch.py
+++ launch_ROS2/Personalized_launch.py
@@ -1,10 +1,8 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-import launch
 import json
 
 # Definições das variáveis
